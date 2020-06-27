dc684f615b5a8952667f033fa40c78a0b1979a1d
Rufaida <eng.rufaidajumaa@gmail.com>
2020-06-27 14:05:54 +0200

    The /meals route  support  query parameters(maxPrice,title)

--- week2/src/backend/index.js
+++ week2/src/backend/index.js
@@ -1,16 +1,32 @@
 
 const express = require("express");
 const app = express();
-const mealsRouter = require("./routes/meals");
-const  reservations = require("./routes/reservations");
-const  reviews = require("./routes/reviews");
-
-
-
+const  mealsRouter = require("./routes/meals");
+const  reservationsRouter = require("./routes/reservations");
+const  reviewsRouter = require("./routes/reviews");
 
 
 app.get("/meals", (req, res) => {
-  res.send(mealsRouter);
+  let correspondingMeals = mealsRouter;
+// Check if there is a max price 
+  if (req.query.maxPrice){
+    correspondingMeals = mealsRouter.filter((meal)=>{
+                          return meal.price <= req.query.maxPrice ;
+                          })
+  }
+//  Check if there is a specific title
+  if (req.query.title){
+    let reg = new RegExp(req.query.title,"i")
+    correspondingMeals= correspondingMeals.filter((meal)=>{
+                          return meal.title.match(reg);
+                          })
+}
+res.send(correspondingMeals);
+});
+
+app.get("/meal/:id", (req, res) => {
+  let correspondingMeal =  mealsRouter.filter((meal)=> meal.id == req.params.id );
+  res.send(correspondingMeal);
 });
 
 
@@ -18,9 +34,22 @@ app.get("/reservations", (req, res) => {
   res.send(reservations);
 });
 
+app.get("/reservation/:id", (req, res) => {
+  let correspondingReservation =  reservationsRouter.filter((reservation)=> reservation.id == req.params.id );
+  res.send(correspondingReservation);
+});
+
 app.get("/reviews", (req, res) => {
   res.send(reviews);
 });
 
+app.get("/review/:id", (req, res) => {
+ let  filterCorrespondingReview =  reviewsRouter.filter((review)=> review.id == req.params.id );
+  let correspondingReview =
+  filterCorrespondingReview.map(review => {
+              return {content: review.content ,numberOfStars:review.numberOfStars, createdAt : review.createdAt}
+             })
+  res.send(correspondingReview);
+});
 
 app.listen(3000,()=>console.log('running'));

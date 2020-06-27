24f667411730c6e6b3231ec94ba448ffe9f933c8
Rufaida <eng.rufaidajumaa@gmail.com>
2020-06-27 14:07:22 +0200

    Delete the empty array

--- week2/src/backend/routes/meals.js
+++ week2/src/backend/routes/meals.js
@@ -1,6 +1,5 @@
 const meals = require("../data/meals.json");
 const reviews = require("../data/reviews.json");
-let result =[];
 let mealsWithReviews = meals.map((meal)=>{ 
     // Find the reviews to the meal
     let mealReviews = reviews.filter((review) => {

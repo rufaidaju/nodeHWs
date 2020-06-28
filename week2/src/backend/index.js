9adda01289b6ac3983fb78a916b8e8ae65f69e69
Rufaida <eng.rufaidajumaa@gmail.com>
2020-06-28 21:07:49 +0200

    Check if there is no meal with the requested id,writes a string as id and a query parameter that is not supported

--- week2/src/backend/index.js
+++ week2/src/backend/index.js
@@ -6,52 +6,95 @@ const  reservationsRouter = require("./routes/reservations");
 const  reviewsRouter = require("./routes/reviews");
 
 
+app.use("/meals",function(req,res,next){
+  const meals =req.query;
+  const maxPrice = req.query.maxPrice;
+  const title=req.query.title ;
+  const createdAfter =req.query.createdAfter;
+  const limit =req.query.limit;
+  // Check if the object is empty
+  function isEmpty(obj) {
+    for (let key in obj) {
+      return false;
+    }
+    return true;
+  }
+  if (maxPrice || title || createdAfter|| limit ||  isEmpty(meals) ){
+    next()
+  }else{
+    res.send(404)
+  }
+
+})
 app.get("/meals", (req, res) => {
   let correspondingMeals = mealsRouter;
+  const maxPrice = Number(req.query.maxPrice);
+  const title=req.query.title ;
+  const createdAfter =req.query.createdAfter;
+  const limit =Number(req.query.limit);
 // Get meals with a max price 
-  if (req.query.maxPrice){
+  if (maxPrice){
     correspondingMeals = mealsRouter.filter((meal)=>{
-                          return meal.price <= req.query.maxPrice ;
+                          return meal.price <= maxPrice ;
                           })
   }
 //  Get meals with  a specific title
-  if (req.query.title){
-    let reqTitle = new RegExp(req.query.title,"i")
+  if (title){
+    let reqTitle = new RegExp(title,"i")
     correspondingMeals= correspondingMeals.filter((meal)=>{
                           return meal.title.match(reqTitle);
                           })
 }
 
 //Get meals that has been created after a specific date
-  if (req.query.createdAfter){
-    let reqCreatedAfter = Date.parse(req.query.createdAfter);
-    correspondingMeals= correspondingMeals.filter((meal)=>{
-                          mealCreatedAfter =  Date.parse(meal.createdAt)
+  if (createdAfter){
+    let reqCreatedAfter = Date.parse(createdAfter);
+    correspondingMeals = correspondingMeals.filter((meal)=>{
+                          mealCreatedAfter =  Date.parse(meal.createdAt) 
                           return mealCreatedAfter - reqCreatedAfter > 0  ;
                           })
-  }
+  }   
  
 // Get specific number of meals
-  if (req.query.limit){
-    correspondingMeals = mealsRouter.slice(0,req.query.limit);
-    console.log(correspondingMeals)
+  if (limit){
+    correspondingMeals = mealsRouter.slice(0,limit);
   }
-res.send(correspondingMeals); 
+
+// Check if there are no meals to show
+  if( correspondingMeals.length == 0 ){
+    res.send("There are no meals to show ")
+  } else{
+    res.send(correspondingMeals);  
+  }
+   
 });
 
 app.get("/meal/:id", (req, res) => {
   let correspondingMeal =  mealsRouter.filter((meal)=> meal.id == req.params.id );
-  res.send(correspondingMeal);
+  const id = req.params.id;
+  if (isNaN(id) ){
+    res.send(`The id ( ${id }) is invalid, it's not a number `)
+  }else if ( correspondingMeal.length == 0 ) {
+    res.send(404)
+  } else {
+    res.send(correspondingMeal);  
+  }
 });
-
 
 app.get("/reservations", (req, res) => {
   res.send(reservations);
 });
 
 app.get("/reservation/:id", (req, res) => {
+  const id = req.params.id;
   let correspondingReservation =  reservationsRouter.filter((reservation)=> reservation.id == req.params.id );
-  res.send(correspondingReservation);
+  if (isNaN(id) ){
+    res.send(`The id ( ${id }) is invalid, it's not a number `)
+  }else if ( correspondingReservation.length == 0 ) {
+    res.send(404)
+  } else {
+    res.send(correspondingReservation);
+  }
 });
 
 app.get("/reviews", (req, res) => {
@@ -59,12 +102,19 @@ app.get("/reviews", (req, res) => {
 });
 
 app.get("/review/:id", (req, res) => {
- let  filterCorrespondingReview =  reviewsRouter.filter((review)=> review.id == req.params.id );
+  const id = req.params.id;
+  let  filterCorrespondingReview =  reviewsRouter.filter((review)=> review.id == req.params.id );
   let correspondingReview =
   filterCorrespondingReview.map(review => {
               return {content: review.content ,numberOfStars:review.numberOfStars, createdAt : review.createdAt}
              })
-  res.send(correspondingReview);
+  if (isNaN(id) ){
+    res.send(`The id ( ${id }) is invalid, it's not a number `)
+  }else if ( correspondingReview.length == 0 ) {
+    res.send(404)
+  } else {
+    res.send(correspondingReview);
+  }
 });
 
-app.listen(3000,()=>console.log('running'));
+app.listen(3000,()=>console.log('running')); 

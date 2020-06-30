cf0fb3c0dfab4a089ef8b8697164dd3a72146b27
Rufaida <eng.rufaidajumaa@gmail.com>
2020-06-30 11:07:45 +0200

    Shorten the function and change the name to reviews

--- week1/src/backend/routes/meals.js
+++ week1/src/backend/routes/meals.js
@@ -1,12 +1,10 @@
 const meals = require("../data/meals.json");
 const reviews = require("../data/reviews.json");
-let result =[];
 let mealsWithReviews = meals.map((meal)=>{ 
-    // Find the reviews to the meal
     let mealReviews = reviews.filter((review) => {
-        return meal.id == review.mealId;
+      return meal.id == review.mealId;
     })
-    meal.review = mealReviews;
+    meal.reviews = mealReviews;
     return meal;
 });
  module.exports = mealsWithReviews;

fa97074e18ab48f4bbce008fb467d3cf22d62f40
Rufaida <eng.rufaidajumaa@gmail.com>
2020-06-30 12:54:45 +0200

    Shorten the code

--- week1/src/backend/routes/meals.js
+++ week1/src/backend/routes/meals.js
@@ -1,6 +1,5 @@
 const meals = require("../data/meals.json");
 const reviews = require("../data/reviews.json");
-let result =[];
 let mealsWithReviews = meals.map((meal)=>{ 
     let mealReviews = reviews.filter((review) => {
         return meal.id == review.mealId;

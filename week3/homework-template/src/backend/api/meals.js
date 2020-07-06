44902d8ebdb9c763dc3307bb97a6a3f8e1652e43
Rufaida <eng.rufaidajumaa@gmail.com>
2020-07-06 13:19:21 +0200

    (Get,Post)meals and (Get,Put)meal by id

--- week3/homework-template/src/backend/api/meals.js
+++ week3/homework-template/src/backend/api/meals.js
@@ -3,14 +3,59 @@ const app = express();
 const router = express.Router();
 const knex = require("../database");
 
+router.get("/:id", async (request, response) => {
+  try {
+    const meal =  await knex.from('meal').select('*')
+    .where('id',request.params.id);
+   
+    if (meal.length == 0){
+      response.send(404);
+    }else{
+      response.json(meal);
+    }
+    
+  } catch (error) {
+    throw error;
+  }
+});
+
 router.get("/", async (request, response) => {
   try {
-    // knex syntax for selecting things. Look up the documentation for knex for further info
-    const titles = await knex("meals").select("title");
+    const titles = await knex("meal").select("title");
     response.json(titles);
   } catch (error) {
     throw error;
   }
 });
 
-module.exports = router;
+router.post("/", async (request, response) => {
+  try {
+    
+    await knex('meal').insert({title:request.body.title,
+      maxNumberOfGuests:request.body.maxNumberOfGuests, 
+      description:request.body.description, 
+      createdAt:request.body.createdAt, 
+      price:request.body.price})  
+      response.send('response')  
+  } catch (error) {
+    throw error;   
+  }
+});   
+
+router.put("/:id", async (request, response) => {
+  try {
+    const meal =  await knex('meal').where('id',request.params.id)
+      .update({title:request.body.title,
+        maxNumberOfGuests:request.body.maxNumberOfGuests,
+        description:request.body.description,
+        createdAt:request.body.createdAt,
+        price:request.body.price
+      });
+    response.send(request.body); 
+    
+  } catch (error) { 
+    throw error; 
+  }
+});
+ 
+module.exports = router;    
